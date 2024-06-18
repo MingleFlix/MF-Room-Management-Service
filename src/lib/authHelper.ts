bb16634aa2396db97500b3a84d82f3efad2a5b38
Jesse Guenzl <contact@jesseguenzl.com>
2024-06-18 21:38:40 +0200

    add auth helper for decoding jwt

---
+++ src/lib/authHelper.ts
@@ -0,0 +1,14 @@
+import jwt from "jsonwebtoken";
+import {Request} from "express";
+
+export interface JWTPayload {
+    userId: string;
+    email: string;
+    username: string;
+}
+
+export function authenticateJWT(token: string): JWTPayload {
+    const secretKey = process.env.JWT_SECRET || 'your_secret_key'; // Ensure you have this in your environment variables
+    const decoded = jwt.verify(token, secretKey);
+    return decoded as JWTPayload;
+}
--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -1,21 +1,23 @@
-import { Request, Response, NextFunction } from 'express';
-import jwt from 'jsonwebtoken';
+import {NextFunction, Request, Response} from 'express';
+import {authenticateJWT, JWTPayload} from "../lib/authHelper";
 
-interface AuthRequest extends Request {
-    user?: any;
+export interface AuthRequest extends Request {
+    user: JWTPayload;
 }
 
-const authMiddleware = (req: AuthRequest, res: Response, next: NextFunction) => {
-    const token = req.headers['authorization']?.split(' ')[1];
+const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
+    const authRequest = req as AuthRequest;
+    const token = authRequest.headers['authorization']?.split(' ')[1];
 
     if (!token) {
         return res.status(401).json({ message: 'Access denied, no token provided' });
     }
 
     try {
-        const secretKey = process.env.JWT_SECRET || 'your_secret_key'; // Ensure you have this in your environment variables
-        const decoded = jwt.verify(token, secretKey);
-        req.user = decoded;
+        (authRequest as AuthRequest).user = authenticateJWT(token);
+        if (!authRequest.user) {
+            throw new Error('Invalid token');
+        }
         next();
     } catch (error) {
         return res.status(401).json({ message: 'Invalid token' });
