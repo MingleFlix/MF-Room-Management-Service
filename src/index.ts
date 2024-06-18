fc192ef3d1dfc99078992df1229594e927393b21
Jesse Guenzl <contact@jesseguenzl.com>
2024-06-19 00:14:33 +0200

    ad websocket

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import express from 'express';
-import {randomUUID} from "node:crypto";
 import WebSocket from "ws";
 import dotenv from 'dotenv';
 import {authenticateJWT} from "./lib/authHelper";
@@ -31,6 +30,46 @@ app.get('/', (req, res) => {
 
 app.use('/rooms', routes);
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
     console.log(`Room Management Service is running on http://localhost:${port}`);
+});
+
+// Websocket server
+const wss = new WebSocket.Server({ server: server});
+
+wss.on('connection', async (ws, req) => {
+    console.log('New WebSocket connection');
+    // Authenticate user via JWT (simplified example)
+    // Parse room ID from query parameters
+    const params = new URLSearchParams(req.url?.substring(1));
+    const roomID = params.get('roomID');
+    const token = params.get('token');
+    console.log('roomID:', roomID);
+    console.log('token:', token);
+    let user: any;
+    try {
+        user = authenticateJWT(token || '');
+    } catch (error) {
+        console.error('Error authenticating user:', error);
+    }
+
+
+    if (!user || !roomID) {
+        ws.close();
+        return;
+    }
+    //check if roomID is valid and in db
+    const room =  await redisClient.hGet('rooms', roomID);
+    if (!room) {
+        ws.close();
+        return;
+    }
+
+    ws.on('message', async (message) => {
+        const event = JSON.parse(message as any);
+        const roomID = event.roomID;
+
+        // Publish the event to Redis
+        await redisClient.publish(roomID, JSON.stringify(event));
+    });
 });
